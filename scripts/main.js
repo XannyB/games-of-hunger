a97a7aae24f9ad0c8b9b5d125dab4b565c927390
XannyB <xannyb@gmail.com>
2025-02-11 14:57:57 +0000

    latest changes

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+function removeCharacters(id, speed, ...params) {
+    const getMainContent = document.getElementById(id);
+    
+    // setInterval runs continuously, per ms, until stopped
+    const intervalId = setInterval(() => {
+        const currentText = getMainContent.textContent;
+        
+        // Check if there's no text left to remove
+        if (currentText.length === 0) {
+            clearInterval(intervalId);
+
+            if (params[0] != undefined) {
+                console.log("Parameters given.");
+
+                removeCharacters(params.shift(), params.shift(), ...params);
+            }
+
+            return;
+        }
+
+        // Remove the last character from the text content
+        getMainContent.textContent = currentText.slice(0, -1);
+    }, speed); // Adjust the delay (in milliseconds) to control the speed
+}
