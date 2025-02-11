f3f036fdc7382c9dcd310ec8cc5f7b08534f75c0
XannyB <xannyb@gmail.com>
2025-02-11 22:39:54 +0000

    latest changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,45 @@
 function removeCharacters(id, speed, ...params) {
     // remove button first
-    if (document.getElementById('button')) {
-        document.getElementById('button').remove();
+    if (document.getElementById("button")) {
+        document.getElementById("button").remove();
     }
     
-    const getMainContent = document.getElementById(id);
+    const getMainContent = document.getElementById(id);  //get by id
+    let currentP = undefined;
+    let mainContent;
+
+    if (getMainContent.getElementsByTagName('p').length > 0) {
+        let pNum = getMainContent.getElementsByTagName('p').length - 1;
+        currentP = getMainContent.getElementsByTagName('p')[pNum];
+        mainContent = currentP;
+    } else {
+        mainContent = getMainContent;
+    }
     
     // setInterval runs continuously, per ms, until stopped
     const intervalId = setInterval(() => {
-        const currentText = getMainContent.textContent;
-        
+        const currentText = mainContent.textContent;
+
         // Check if there's no text left to remove
+
         if (currentText.length === 0) {
+
             clearInterval(intervalId);
 
+            if (currentP) {
+                currentP.remove();
+                removeCharacters(id, speed, ...params);
+                return;
+            }
+
             if (params[0] != undefined) {
-                removeCharacters(params.shift(), params.shift(), ...params);
+                removeCharacters(params.shift(), params.shift(), ...params); //Takes a function, and one argument (speed)
             }
 
             return;
         }
 
         // Remove the last character from the text content
-        getMainContent.textContent = currentText.slice(0, -1);
+        mainContent.textContent = currentText.slice(0, -1);
     }, speed); // Adjust the delay (in milliseconds) to control the speed
 }
